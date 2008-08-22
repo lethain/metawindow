cdb57819bc5f0b0edce033c449ef992e3fd92a28
Will Larson <lethain@gmail.com>
2008-08-22 14:44:04 -0400

    Added some methods to app delegate.

--- MetaWindowAppDelegate.py
+++ MetaWindowAppDelegate.py
@@ -12,3 +12,19 @@ from AppKit import *
 class MetaWindowAppDelegate(NSObject):
     def applicationDidFinishLaunching_(self, sender):
         NSLog("Application did finish launching.")
+    
+    def applicationWillTerminate_(self,sender):
+        NSLog("Application will terminate.")
+        
+    def applicationSupportFolder(self):
+        paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory,NSUserDomainMask,True)
+        basePath = (len(paths) > 0 and paths[0]) or NSTemporaryDirectory()
+        fullPath = basePath.stringByAppendingPathComponent_("MetaWindow")
+        if not os.path.exists(fullPath):
+            os.mkdir(fullPath)
+        return fullPath
+        
+    def pathForFile(self,filename):
+        return self.applicationSupportFolder().stringByAppendingPathComponent_(filename)
+        
+    NSApplication.sharedApplication().delegate()
