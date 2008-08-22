914e4f83be78a5af8af7237b8e917c52b8418d8c
Will Larson <lethain@gmail.com>
2008-08-22 14:44:16 -0400

    Created MWController.py file.

---
+++ MWController.py
@@ -0,0 +1,12 @@
+#
+#  MWController.py
+#  MetaWindow
+#
+#  Created by Will Larson on 8/22/08.
+#  Copyright (c) 2008 Will Larson. All rights reserved.
+#
+
+from Foundation import *
+
+class MWController(NSObject):
+    pass
