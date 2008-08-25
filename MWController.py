c344ca385b7650cbd50427a11ea0af78610bfa9a
Will Larson <lethain@gmail.com>
2008-08-25 12:52:50 -0400

    Now caches results correctly.

--- MWController.py
+++ MWController.py
@@ -6,14 +6,23 @@
 #  Copyright (c) 2008 Will Larson. All rights reserved.
 #
 
-import objc, metaweb, webbrowser
+import objc, metaweb, webbrowser, pickle, datetime, md5
+from AppKit import *
 from Foundation import *
+
 
 class MWController(NSObject):
     tableView = objc.IBOutlet()
     textField = objc.IBOutlet()
     arrayController = objc.IBOutlet()
     results = []
+    _cache = None
+    
+    def getCache(self):
+        if self._cache is None:
+            self._cache = NSApp.delegate().cache
+        return self._cache
+    cache = property(getCache,None,None,"Cache of searches.")
     
     def awakeFromNib(self):
         if self.tableView:
@@ -34,12 +43,34 @@ class MWController(NSObject):
         
         url = u"http://www.freebase.com/view%s" % row['id']
         webbrowser.open(url)
+    
+    def getCachedSearch(self,searchString):
+        if self.cache.has_key(searchString):
+            filename,timestamp = self.cache[searchString]
+            age = datetime.datetime.now() - timestamp
+            if age > datetime.timedelta(days=1):
+                return None
+            filepath = NSApp.delegate().pathForFile(filename)
+            file = open(filepath,'r')
+            data = pickle.load(file)
+            file.close()
+            return data
+        return None
         
+    def cacheResultsForSearch(self,searchString,results):
+        filename = u"%s.cached" % md5.md5(searchString).hexdigest()[12:]
+        filepath = NSApp.delegate().pathForFile(filename)
+        file = open(filepath,'w')
+        pickle.dump(results,file)
+        file.close()
+        self.cache[searchString] = (filename,datetime.datetime.now())
     
     @objc.IBAction
     def search_(self,sender):
         search_value = self.textField.stringValue()
-        lst = metaweb.search(search_value)
-        self.results = [ NSDictionary.dictionaryWithDictionary_(x) for x in lst]
-        NSLog(u"results: %s" % self.results)
+        cached = self.getCachedSearch(search_value)
+        if cached is None:
+            cached = metaweb.search(search_value)
+            self.cacheResultsForSearch(search_value,cached)
+        self.results = [ NSDictionary.dictionaryWithDictionary_(x) for x in cached]
         self.arrayController.rearrangeObjects()
--- MetaWindowAppDelegate.py
+++ MetaWindowAppDelegate.py
@@ -6,16 +6,26 @@
 #  Copyright Will Larson 2008. All rights reserved.
 #
 
+import os,pickle
 from Foundation import *
 from AppKit import *
 
 class MetaWindowAppDelegate(NSObject):
     def applicationDidFinishLaunching_(self, sender):
-        NSLog("Application did finish launching.")
+        try:
+            path = self.pathForFile('cache.serialized')
+            file = open(path,'r')
+            self.cache = pickle.load(file)
+            file.close()
+        except IOError:
+            self.cache = {}
     
     def applicationWillTerminate_(self,sender):
-        NSLog("Application will terminate.")
-        
+        path = self.pathForFile('cache.serialized')
+        file = open(path,'w')
+        pickle.dump(self.cache,file)
+        file.close()
+                
     def applicationSupportFolder(self):
         paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory,NSUserDomainMask,True)
         basePath = (len(paths) > 0 and paths[0]) or NSTemporaryDirectory()
