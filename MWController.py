191b6dba4377f6911bbeffa1a6bbb95ff3e73f1c
Will Larson <lethain@gmail.com>
2008-08-25 09:21:37 -0400

    Double click now opens row in webpage (project 1 in segment 3).

--- MWController.py
+++ MWController.py
@@ -6,7 +6,7 @@
 #  Copyright (c) 2008 Will Larson. All rights reserved.
 #
 
-import objc, metaweb
+import objc, metaweb, webbrowser
 from Foundation import *
 
 class MWController(NSObject):
@@ -15,9 +15,31 @@ class MWController(NSObject):
     arrayController = objc.IBOutlet()
     results = []
     
+    def awakeFromNib(self):
+        if self.tableView:
+            self.tableView.setTarget_(self)
+            self.tableView.setDoubleAction_("open:")
+    
+    def open_(self,sender):
+        selectedObjs = self.arrayController.selectedObjects()
+        if len(selectedObjs) == 0:
+            NSLog(u"No selected row!")
+            return
+            
+        row = selectedObjs[0]
+        NSLog(u"Row: %s" % row)
+        if not row.has_key('id') or row['id'] == None:
+            NSLog(u"Row has no id!")
+            return
+        
+        url = u"http://www.freebase.com/view%s" % row['id']
+        webbrowser.open(url)
+        
+    
     @objc.IBAction
     def search_(self,sender):
         search_value = self.textField.stringValue()
         lst = metaweb.search(search_value)
         self.results = [ NSDictionary.dictionaryWithDictionary_(x) for x in lst]
+        NSLog(u"results: %s" % self.results)
         self.arrayController.rearrangeObjects()
